0647c4a1d4a8b4470ade791574e0fbe3c84e2044
miller862 <43028862manu@gmail.com>
2022-09-20 20:18:49 -0300

    probando

---
+++ NumpyPRUEBA.py
@@ -0,0 +1,27 @@
+#import numpy as np
+
+#6- Crear una matriz de 5x5 con el valor 1 en todos sus bordes y 0 en su interior.
+
+#arr14= np.ones((5,5,3))
+#arr11b= np.full((5,5), 2)
+#print(arr11)
+#print(arr11b)
+#arr11*arr1
+#mayores=[]
+#arr14= np.random.randint(100,size=(5,5,3))
+#print(arr14)
+#n= int(input("ingresa un entero y encuentra esa cantidad de numeros maximos"))
+#for i in range(n):
+#    mayores= mayores.append(np.max(arr14))
+#print(mayores)
+
+import numpy as np
+
+
+
+
+
+
+
+
+
